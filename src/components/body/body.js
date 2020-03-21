e281f6834e9fec8c31d9fdc9002ad3e2c5b0344e
Hoomie Janell <hoomiejanell@gmail.com>
2020-03-21 12:31:07 -0500

    front-end

---
+++ src/components/body/body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./body.css";
+
+function Footer() {
+  return (
+    <div className="row" className="body">
+      <div className="col"></div>
+    </div>
+  );
+}
+
+export default Footer;

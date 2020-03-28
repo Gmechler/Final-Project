1c7aa4710690bbb715d0c8a3ebec987a932fdd56
Devin Alexander <alexanderdev117@gmail.com>
2020-03-28 10:16:31 -0500

    ready for pull

--- client/src/components/body/body.js
+++ client/src/components/body/body.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./body.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import RecipePage from "../../pages/recipie";
+import RecipePage from "../../pages/recipe";
 import SearchResults from "../../pages/search-results";
 import Favorites from "../../pages/favorites";
 import ShoppingList from "../../pages/ShoppingList";
@@ -15,6 +15,7 @@ function Body() {
         <Route exact path="/search-results" component={SearchResults} />
         <Route exact path="/favorites" component={Favorites} />
         <Route exact path="/shopping-list" component={ShoppingList} />
+        {/* <Route path="/recipe" component={RecipePage} /> */}
       </div>
     </div>
     </Router>
--- server.js
+++ server.js
@@ -11,7 +11,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 //adds the routes
-require("./routes/user-api-route");
+require("./routes/user-api-route")(app);
+require("./routes/recipe-route")(app);
 //sets up the connection and makes the database
 //if force: true is present will always overide your current database
 db.sequelize.sync({ force: true }).then(function() {
